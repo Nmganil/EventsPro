949469155a184fd5b2fdc95df73aed1e6b81441d
anil gurung <anilgrg23.ag@gmail.com>
2023-09-19 15:37:55 +0100

    branch A commited

--- EventsPro/Controllers/OrganizerController.cs
+++ EventsPro/Controllers/OrganizerController.cs
@@ -4,7 +4,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 // This is also changed in Branch "A". Some more changes
-//this is branch A
+//this is branch A.Testing stash
 namespace EventsPro.Controllers
 {
     [Route("api/[controller]")]
